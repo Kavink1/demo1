4c7867739a9bfa6a8382bb380bb159fc2b7074b7
admin <admin@DESKTOP-A7U52L2>
2022-06-22 12:26:52 +0530

    Load

--- Coding/src/demo/Demo.java
+++ Coding/src/demo/Demo.java
@@ -2,8 +2,39 @@ package demo;
 
 public class Demo {
 	public static void main(String[] args) {
-		String s1="The box is {height} inches high and {length} inches in length";
-		String n = s1.replaceAll("[{}]", " ");
-		System.out.println(n);
+		String a="18";
+		int d = Integer.parseInt(a);
+		System.out.println(d);
+		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
